907b8e4ac58d2daf8fee18ce30c6c3107021ce11
Matthew <matthewhcbates@gmail.com>
2022-04-15 16:00:46 -0600

    updated email and resume

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -11,34 +11,34 @@ export default function Introduction() {
               <div className="container-fluid">
                 <div className="row">
                   <div className="col-sm-12 col-xs-12 js-fullheight slider-text">
-                  <div className="slider-text-inner js-fullheight">
-                    <div className="desc">
-                      <div>
-                        <h1>Hi, I'm Matthew.</h1>
-                        <h4>A creative, passionate technologist.</h4>
-                        <p>
-                          <a
-                            className="btn btn-primary btn-learn"
-                            href="https://drive.google.com/file/d/15jd7IKnp0kOCxmMwcz06eXhR68WrXsXX/view?usp=sharing"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            View CV
-                          </a>{" "}
-                          <a
-                            className="btn btn-primary btn-learn"
-                            href="https://medium.com/@matthewhcbates"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            View Recent Blog Posts
-                          </a>
-                        </p>
+                    <div className="slider-text-inner js-fullheight">
+                      <div className="desc">
+                        <div>
+                          <h1>Hi, I'm Matthew.</h1>
+                          <h4>A creative, passionate technologist.</h4>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://drive.google.com/file/d/1GllLJYSQkUxqrYnB1592LVSML7BJyPE2/view?usp=sharing"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View CV
+                            </a>{" "}
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://medium.com/@matthewhcbates"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Recent Blog Posts
+                            </a>
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
               </div>
             </li>
           </ul>
