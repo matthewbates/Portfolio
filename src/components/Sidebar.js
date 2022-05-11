db72302f1c0380bf908d5ccd4105ae14330254f2
Matthew <matthewhcbates@gmail.com>
2022-05-11 11:43:59 -0600

    aligned sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -107,32 +107,6 @@ export default function Sidebar() {
               </div>
             </nav>
             <br />
-            {/* <nav id="colorlib-main-menu" role="navigation" className="navbar">
-              <SocialIcon
-                url="https://www.linkedin.com/in/matthew-bates-71b7bb79/"
-                target="_blank"
-              />{" "}
-              <a
-                href="https://codewars.com/users/matthewhcbates"
-                target="_blank"
-                className="codewars"
-                rel="noopener noreferrer"
-              >
-                <img className="nav-icon-codewars" src={codeWars} alt="icon" />
-              </a>
-              <SocialIcon
-                url="https://github.com/matthewbates"
-                target="_blank"
-              />{" "}
-              <a
-                href="https://medium.com/@matthewhcbates"
-                target="_blank"
-                className="nav-icon"
-                rel="noopener noreferrer"
-              >
-                <img className="nav-icon" src={medium} alt="icon" />
-              </a>
-            </nav> */}
           </div>
         </aside>
       </div>
